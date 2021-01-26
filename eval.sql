8162efabdc23de0e9e07c976ed8a62122dc8e550
binyoma <binyomainnocent@gmail.com>
2021-01-26 20:49:56 +0100

    add trigger

--- eval.sql
+++ eval.sql
@@ -8,13 +8,13 @@ contactName as "contact",
 ContactTitle as "Fonction",
 Phone as "Téléphone"
 FROM customers
-WHERE Country="France"
+WHERE Country="France";
 
 /* 2 - Produits vendus par le fournisseur « Exotic Liquids »*/
 SELECT ProductName as "Produit",
 UnitPrice as "Prix"
 FROM products
-WHERE SupplierID=1
+WHERE SupplierID=1;
 
 /*3 - Nombre de produits vendus par les fournisseurs Français dans l’ordre décroissant*/
 SELECT CompanyName as "Fournisseur",
@@ -22,7 +22,7 @@ count(products.SupplierID) as "Nbre produits"
 FROM suppliers INNER JOIN products on suppliers.SupplierID=products.SupplierID
 WHERE suppliers.Country="France"
 group by products.SupplierID
-order by count(products.SupplierID) Desc  
+order by count(products.SupplierID) Desc  ;
 
 /*4 Liste des clients Français ayant plus de 10 commandes*/
 SELECT CompanyName as "client",
@@ -30,7 +30,7 @@ COUNT(orders.CustomerID) as "Nbre commandes"
 FROM orders inner join customers on orders.CustomerID=customers.CustomerID
 WHERE ShipCountry ="France"
 group by orders.CustomerID
-Having COUNT(orders.CustomerID) >10 
+Having COUNT(orders.CustomerID) >10 ;
 
 /*5 - Liste des clients ayant un chiffre d’affaires > 30.00*/
 SELECT ShipName as "client",
@@ -39,7 +39,7 @@ ShipCountry as Pays
 FROM  orders INNER JOIN orderdetails ON orders.OrderID=orderdetails.OrderID
 GROUP BY CLIENT
 HAVING CA>30000
-ORDER BY CA DESC 
+ORDER BY CA DESC ;
 
 /*6 – Liste des pays dont les clients ont passé commande de produits fournis par « Exotic
 Liquids »*/
@@ -49,7 +49,7 @@ FROM customers inner join orders on customers.CustomerID=orders.CustomerID
     INNER JOIN products  on   orderdetails.productID=products.productID
     INNER JOIN suppliers on products.SupplierID=suppliers.supplierID
 Where suppliers.CompanyName="Exotic Liquids"
-ORDER BY customers.Country Asc
+ORDER BY customers.Country Asc;
 
 /*7 – Montant des ventes de 1997 */
 SELECT SUM(orderdetails.UnitPrice*orderdetails.Quantity) as "Montant ventes 97"
@@ -60,16 +60,16 @@ WHERE YEAR(OrderDate)=1997
 SELECT SUM(orderdetails.UnitPrice*orderdetails.Quantity) as "Montant ventes 97"
 FROM orders inner JOIN orderdetails on orders.OrderID=orderdetails.OrderID
 WHERE YEAR(OrderDate)= 1997
-GROUP BY MONTH(orderDate)
+GROUP BY MONTH(orderDate);
 
 /*9 – Depuis quelle date le client « Du monde entier » n’a plus commandé ?*/
 SELECT MAX(OrderDate) as "Date de dernière commande"
 FROM orders inner JOIN customers on orders.CustomerID=customers.CustomerID
-WHERE customers.CompanyName="Du monde entier"
+WHERE customers.CompanyName="Du monde entier";
 
 /*10 – Quel est le délai moyen de livraison en jours ?*/
 SELECT ROUND(AVG(DATEDIFF(ShippedDate,OrderDate))) AS "Délai moyen de livraison"
-FROM orders
+FROM orders;
 
 
 -- 2) Procédures stockées
@@ -100,5 +100,17 @@ END |
 DELIMITER ;
 
 -- 3)Mise en place d'une règle de gestion
+/*  j'ai proposé d'augmenter le prix de fret de 5%*/
 
-"Avec un trigger qui se declencherait quand le fournisseur et le client n'habitent pas dans un même pays"
+CREATE TRIGGER `transport` AFTER INSERT ON `orderdetails` FOR EACH ROW BEGIN 
+  DECLARE customerCountry VARCHAR(15);
+  DECLARE supplierCountry VARCHAR(15);
+  SET customerCountry = ( SELECT Country FROM customers INNER JOIN orders on customers.CustomerID = orders.CustomerID INNER JOIN orderdetails on orders.OrderID= orderdetails.OrderID WHERE orderdetails.OrderID =NEW.OrderID LIMIT 1); 
+  SET supplierCountry = ( SELECT Country FROM suppliers INNER JOIN products on suppliers.SupplierID = products.SupplierID INNER JOIN orderdetails on products.ProductID= orderdetails.ProductID WHERE orderdetails.ProductID =new.ProductID LIMIT 1); 
+  IF customerCountry != supplierCountry 
+   THEN 
+    UPDATE orders
+    SET Freight= Freight*1.05
+    WHERE OrderID=NEW.OrderID;
+   END IF; 
+END
